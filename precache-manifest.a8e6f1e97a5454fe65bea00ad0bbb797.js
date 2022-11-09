f14fb42b26da098c77281d589e2bb7e101b1f78b
saakshi <saakshisinghal14@gmail.com>
2022-11-09 07:09:01 +0530

    Updates

---
+++ precache-manifest.a8e6f1e97a5454fe65bea00ad0bbb797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "47a3575a991642afa37f",
+    "url": "/Glory-News/static/css/main.a12a3fa3.chunk.css"
+  },
+  {
+    "revision": "47a3575a991642afa37f",
+    "url": "/Glory-News/static/js/main.47a3575a.chunk.js"
+  },
+  {
+    "revision": "271d1efb744527e0afa6",
+    "url": "/Glory-News/static/js/1.271d1efb.chunk.js"
+  },
+  {
+    "revision": "485d3595dcfeb57cf704",
+    "url": "/Glory-News/static/js/2.485d3595.chunk.js"
+  },
+  {
+    "revision": "fdd2bb4e3852326adfd9",
+    "url": "/Glory-News/static/js/runtime~main.fdd2bb4e.js"
+  },
+  {
+    "revision": "eacff094aae6e5f872d1cde490d23d29",
+    "url": "/Glory-News/static/media/loading.eacff094.gif"
+  },
+  {
+    "revision": "d98cd2f3e3cce89d671fa9206f34abc9",
+    "url": "/Glory-News/index.html"
+  }
+];
